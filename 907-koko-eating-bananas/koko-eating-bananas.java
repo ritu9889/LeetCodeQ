b425596749044c50fdd6e3b2ef5387d082e43374
manjul maurya <ritumaurya061@gmail.com>
2026-07-14 15:49:19 +0530

    Time: 11 ms (55.28%) | Memory: 48.1 MB (26.66%) - LeetSync

---
+++ 907-koko-eating-bananas/koko-eating-bananas.java
@@ -0,0 +1,45 @@
+class Solution {
+    static boolean isValidAnswer(int[] arr, int hours, int speed){
+        int h = 0;
+        int i = 0;
+        int val = 0;
+        while(i < arr.length){
+            if(arr[i] % speed == 0){
+                h = h + (arr[i]/speed);
+            }
+            else{
+                h = h + (arr[i]/speed + 1);
+            }
+            if(h > hours){
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int start = 1;
+        int maxVal = -1;
+        for(int i = 0; i < piles.length; i++){
+            if(piles[i] >= maxVal){
+                maxVal = piles[i];
+            }
+        }
+        int end = maxVal;
+        int ans = -1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(isValidAnswer(piles, h, mid)){
+                ans = mid;
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
