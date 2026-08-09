229e43c6beee1ff17ccea19e6426f17ea974d63e
manjul maurya <ritumaurya061@gmail.com>
2026-08-09 22:32:53 +0530

    Time: 53 ms (5.90%) | Memory: 66 MB (25.40%) - LeetSync

---
+++ 2600-maximum-tastiness-of-candy-basket/maximum-tastiness-of-candy-basket.java
@@ -0,0 +1,37 @@
+class Solution {
+    static boolean isValidTaste(int[] price, int k, int taste){
+        int countCandy = 1;
+        int candyPos = 0;
+        for(int i = 1; i < price.length; i++){
+            if(Math.abs(price[i] - price[candyPos]) >= taste){
+                countCandy++;
+                candyPos = i;
+                if(countCandy == k){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    public int maximumTastiness(int[] price, int k) {
+        Arrays.sort(price);
+        int n = price.length;
+
+        int start = 0;
+        int end = price[n-1] - price[0];
+        int ans = 0;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(isValidTaste(price,k,mid)){
+                ans = mid;
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1; 
+            }
+        }
+        return ans;
+    }
+}
