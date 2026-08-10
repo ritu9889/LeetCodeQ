ac0e0363ddb3430987cca0e42e0275ed65ed470a
manjul maurya <ritumaurya061@gmail.com>
2026-08-10 16:47:47 +0530

    Time: 29 ms (34.02%) | Memory: 50.8 MB (94.55%) - LeetSync

---
+++ 1408-find-the-smallest-divisor-given-a-threshold/find-the-smallest-divisor-given-a-threshold.java
@@ -0,0 +1,39 @@
+class Solution {
+    static boolean isValidDivisor(int[] nums, int threshold, int divisor){
+        double sumOfResults = 0;
+        double r = (double)divisor;
+        for(int num : nums){
+            double k = (double)num;
+            double a = k / r;
+            double Quotient = Math.ceil(a);
+            sumOfResults += Quotient;
+        }
+        if((int)sumOfResults <= threshold){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    public int smallestDivisor(int[] nums, int threshold) {
+        int start = 1;
+        int maxi = -1;
+        for(int num : nums){
+            maxi = Math.max(maxi, num);
+        }
+        int end = maxi;
+        int ans = 0;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(isValidDivisor(nums, threshold, mid)){
+                ans = mid;
+                end = mid - 1;
+            }
+            else{
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
